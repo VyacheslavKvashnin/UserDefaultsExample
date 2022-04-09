339f1686a765a27236d41f2f2aa3ac59cb037854
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-09 14:02:26 +0500

    Initial Commit

---
+++ UserDefaultsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsExample
+//
+//  Created by Вячеслав Квашнин on 09.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
